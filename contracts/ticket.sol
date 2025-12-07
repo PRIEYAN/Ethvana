b2718f2a545e3172df5ecc07a86e5323cc5e6bc2
PRIEYAN <mnprieyan@gmail.com>
2025-12-07 11:58:43 +0530

    ticket and user struct

--- contracts/ticket.sol
+++ contracts/ticket.sol
@@ -1,5 +1,45 @@
 pragma solidity ^0.8.0;
 
 contract Ticket{
-    
+
+    struct eventDetails{
+        string eventName;
+        string eventDate;
+        string otherDetails;
+    }
+    struct organiser{
+        address organiserId;
+        string organiserName;
+        string[] prviousEvents;
+    }
+    struct schedule{
+        uint256 allowed;
+        string shortListDate;
+    }
+    struct payment{
+        uint256 amount;
+        address[] paid;
+    }
+    struct ticket {
+        string ticketId;
+        eventDetails EventDetails;
+        organiser Organiser;
+        bool scheduleBooking; // default false
+        schedule Schedule;
+        uint256 maxBooking;
+        //mapping(address => string) bookedBy;
+        payment Payment;
+    }
+
+
+    struct AttendedHistory{
+        uint256[] attended;
+        uint256[] notAttended;
+    }
+    struct user {
+        address userId;
+        AttendedHistory ethvanaEvent;
+    }
+
+
 }
